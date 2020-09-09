e317ad2f25f96f405ebe1a110e764824145adb4e
Shazwi Suwandi <supershazwi@gmail.com>
2020-09-09 21:46:07 +0800

    upadte

--- pages/destination/[destinationSlug]/average-salary/role/[roleSlug].js
+++ pages/destination/[destinationSlug]/average-salary/role/[roleSlug].js
@@ -20,23 +20,108 @@ export default function AdditionalBuyersStampDuty() {
      	          	<div className="col-12 col-md-8">
 	     	            <div className="card card-border border-bleed shadow-light-lg mb-6 mb-md-8 aos-init aos-animate" data-aos="fade-up">
 	     	            	<div className="card-body">
+                        <div className="list-group list-group-flush">
+                          <div className="list-group-item d-flex align-items-center">
+                            <div className="mr-auto">
+                              <Link href="/destination/singapore/average-salary/role/software-developer"><a>
+                              <p className="font-weight-bold mb-1">
+                                Average Salary
+                              </p>
+                              </a></Link>
+                            </div>
+                            <p className="font-weight-bold mb-1">
+                              61,236 SGD per year
+                            </p>
+                          </div>
+                        </div>
+                        <hr/>
 	     	            		<table className="table table-striped" style={{ display: "block", overflow: "auto" }}>
 
   		                      <tr>
-  		                        <th rowspan="2" style={{ backgroundColor: "#2c3e50", color: "white", border: "1px solid #f9fbfd", verticalAlign: "middle" }}>Industry</th>
-  		                        <th scope="col" colspan="3" style={{ backgroundColor: "#2c3e50", color: "white", textAlign: "center", border: "1px solid #f9fbfd" }}>Basic Wage</th>
-  		                        <th scope="col" colspan="3" style={{ backgroundColor: "#2c3e50", color: "white", textAlign: "center", border: "1px solid #f9fbfd" }}>Gross Wage</th>
+  		                        <th rowspan="2" style={{ backgroundColor: "#2c3e50", color: "white", border: "1px solid #161c2d", verticalAlign: "middle" }}>Industry</th>
+  		                        <th scope="col" colspan="3" style={{ backgroundColor: "#2c3e50", color: "white", textAlign: "center", border: "1px solid #161c2d" }}>Basic Wage (Monthly)</th>
+  		                        <th scope="col" colspan="3" style={{ backgroundColor: "#2c3e50", color: "white", textAlign: "center", border: "1px solid #161c2d" }}>Gross Wage (Monthly)</th>
   		                      </tr>
 
 
   		                      <tr>
-  		                        <td style={{ backgroundColor: "#34495e", color: "white", textAlign: "center", border: "1px solid #f9fbfd", padding: "0.5rem", verticalAlign: "middle" }}>25<sup>th</sup> Percentile</td>
-                              <td style={{ backgroundColor: "#34495e", color: "white", textAlign: "center", border: "1px solid #f9fbfd", padding: "0.5rem", verticalAlign: "middle" }}>Median</td>
-  		                        <td style={{ backgroundColor: "#34495e", color: "white", textAlign: "center", border: "1px solid #f9fbfd", padding: "0.5rem", verticalAlign: "middle" }}>75<sup>th</sup> Percentile</td>
-                              <td style={{ backgroundColor: "#34495e", color: "white", textAlign: "center", border: "1px solid #f9fbfd", padding: "0.5rem", verticalAlign: "middle" }}>25<sup>th</sup> Percentile</td>
-                              <td style={{ backgroundColor: "#34495e", color: "white", textAlign: "center", border: "1px solid #f9fbfd", padding: "0.5rem", verticalAlign: "middle" }}>Median</td>
-                              <td style={{ backgroundColor: "#34495e", color: "white", textAlign: "center", border: "1px solid #f9fbfd", padding: "0.5rem", verticalAlign: "middle" }}>75<sup>th</sup> Percentile</td>
+  		                        <td style={{ backgroundColor: "#34495e", color: "white", textAlign: "center", border: "1px solid #161c2d", padding: "0.5rem", verticalAlign: "middle" }}>25<sup>th</sup> Percentile</td>
+                              <td style={{ backgroundColor: "#34495e", color: "white", textAlign: "center", border: "1px solid #161c2d", padding: "0.5rem", verticalAlign: "middle" }}>Median</td>
+  		                        <td style={{ backgroundColor: "#34495e", color: "white", textAlign: "center", border: "1px solid #161c2d", padding: "0.5rem", verticalAlign: "middle" }}>75<sup>th</sup> Percentile</td>
+                              <td style={{ backgroundColor: "#34495e", color: "white", textAlign: "center", border: "1px solid #161c2d", padding: "0.5rem", verticalAlign: "middle" }}>25<sup>th</sup> Percentile</td>
+                              <td style={{ backgroundColor: "#34495e", color: "white", textAlign: "center", border: "1px solid #161c2d", padding: "0.5rem", verticalAlign: "middle" }}>Median</td>
+                              <td style={{ backgroundColor: "#34495e", color: "white", textAlign: "center", border: "1px solid #161c2d", padding: "0.5rem", verticalAlign: "middle" }}>75<sup>th</sup> Percentile</td>
   		                      </tr>
+
+                            <tr>
+                              <td class="font-size-sm" style={{ textAlign: "center", border: "1px solid #161c2d", padding: "0.5rem", verticalAlign: "middle" }}>All</td>
+                              <td class="font-size-sm" style={{ textAlign: "center", border: "1px solid #161c2d", padding: "0.5rem", verticalAlign: "middle" }}>4,000</td>
+                              <td class="font-size-sm" style={{ textAlign: "center", border: "1px solid #161c2d", padding: "0.5rem", verticalAlign: "middle" }}>5,159</td>
+                              <td class="font-size-sm" style={{ textAlign: "center", border: "1px solid #161c2d", padding: "0.5rem", verticalAlign: "middle" }}>6,774</td>
+                              <td class="font-size-sm" style={{ textAlign: "center", border: "1px solid #161c2d", padding: "0.5rem", verticalAlign: "middle" }}>4,120</td>
+                              <td class="font-size-sm" style={{ textAlign: "center", border: "1px solid #161c2d", padding: "0.5rem", verticalAlign: "middle" }}>5,300</td>
+                              <td class="font-size-sm" style={{ textAlign: "center", border: "1px solid #161c2d", padding: "0.5rem", verticalAlign: "middle" }}>7,164</td>
+                            </tr>
+
+                            <tr>
+                              <td class="font-size-sm" style={{ textAlign: "center", border: "1px solid #161c2d", padding: "0.5rem", verticalAlign: "middle" }}>Manufacturing</td>
+                              <td class="font-size-sm" style={{ textAlign: "center", border: "1px solid #161c2d", padding: "0.5rem", verticalAlign: "middle" }}>5,314</td>
+                              <td class="font-size-sm" style={{ textAlign: "center", border: "1px solid #161c2d", padding: "0.5rem", verticalAlign: "middle" }}>6,457</td>
+                              <td class="font-size-sm" style={{ textAlign: "center", border: "1px solid #161c2d", padding: "0.5rem", verticalAlign: "middle" }}>8,011</td>
+                              <td class="font-size-sm" style={{ textAlign: "center", border: "1px solid #161c2d", padding: "0.5rem", verticalAlign: "middle" }}>5,486</td>
+                              <td class="font-size-sm" style={{ textAlign: "center", border: "1px solid #161c2d", padding: "0.5rem", verticalAlign: "middle" }}>6,826</td>
+                              <td class="font-size-sm" style={{ textAlign: "center", border: "1px solid #161c2d", padding: "0.5rem", verticalAlign: "middle" }}>8,051</td>
+                            </tr>
+
+                            <tr>
+                              <td class="font-size-sm" style={{ textAlign: "center", border: "1px solid #161c2d", padding: "0.5rem", verticalAlign: "middle" }}>Wholesale & Retail Trade</td>
+                              <td class="font-size-sm" style={{ textAlign: "center", border: "1px solid #161c2d", padding: "0.5rem", verticalAlign: "middle" }}>3,355</td>
+                              <td class="font-size-sm" style={{ textAlign: "center", border: "1px solid #161c2d", padding: "0.5rem", verticalAlign: "middle" }}>4,800</td>
+                              <td class="font-size-sm" style={{ textAlign: "center", border: "1px solid #161c2d", padding: "0.5rem", verticalAlign: "middle" }}>8,341</td>
+                              <td class="font-size-sm" style={{ textAlign: "center", border: "1px solid #161c2d", padding: "0.5rem", verticalAlign: "middle" }}>3,550</td>
+                              <td class="font-size-sm" style={{ textAlign: "center", border: "1px solid #161c2d", padding: "0.5rem", verticalAlign: "middle" }}>4,995</td>
+                              <td class="font-size-sm" style={{ textAlign: "center", border: "1px solid #161c2d", padding: "0.5rem", verticalAlign: "middle" }}>8,381</td>
+                            </tr>
+
+                            <tr>
+                              <td class="font-size-sm" style={{ textAlign: "center", border: "1px solid #161c2d", padding: "0.5rem", verticalAlign: "middle" }}>Transportation & Storage</td>
+                              <td class="font-size-sm" style={{ textAlign: "center", border: "1px solid #161c2d", padding: "0.5rem", verticalAlign: "middle" }}>4,000</td>
+                              <td class="font-size-sm" style={{ textAlign: "center", border: "1px solid #161c2d", padding: "0.5rem", verticalAlign: "middle" }}>4,325</td>
+                              <td class="font-size-sm" style={{ textAlign: "center", border: "1px solid #161c2d", padding: "0.5rem", verticalAlign: "middle" }}>5,262</td>
+                              <td class="font-size-sm" style={{ textAlign: "center", border: "1px solid #161c2d", padding: "0.5rem", verticalAlign: "middle" }}>4,005</td>
+                              <td class="font-size-sm" style={{ textAlign: "center", border: "1px solid #161c2d", padding: "0.5rem", verticalAlign: "middle" }}>4,620</td>
+                              <td class="font-size-sm" style={{ textAlign: "center", border: "1px solid #161c2d", padding: "0.5rem", verticalAlign: "middle" }}>5,766</td>
+                            </tr>
+
+                            <tr>
+                              <td class="font-size-sm" style={{ textAlign: "center", border: "1px solid #161c2d", padding: "0.5rem", verticalAlign: "middle" }}>Information & Communications</td>
+                              <td class="font-size-sm" style={{ textAlign: "center", border: "1px solid #161c2d", padding: "0.5rem", verticalAlign: "middle" }}>3,974</td>
+                              <td class="font-size-sm" style={{ textAlign: "center", border: "1px solid #161c2d", padding: "0.5rem", verticalAlign: "middle" }}>4,808</td>
+                              <td class="font-size-sm" style={{ textAlign: "center", border: "1px solid #161c2d", padding: "0.5rem", verticalAlign: "middle" }}>5,800</td>
+                              <td class="font-size-sm" style={{ textAlign: "center", border: "1px solid #161c2d", padding: "0.5rem", verticalAlign: "middle" }}>4,105</td>
+                              <td class="font-size-sm" style={{ textAlign: "center", border: "1px solid #161c2d", padding: "0.5rem", verticalAlign: "middle" }}>5,070</td>
+                              <td class="font-size-sm" style={{ textAlign: "center", border: "1px solid #161c2d", padding: "0.5rem", verticalAlign: "middle" }}>6,300</td>
+                            </tr>
+
+                            <tr>
+                              <td class="font-size-sm" style={{ textAlign: "center", border: "1px solid #161c2d", padding: "0.5rem", verticalAlign: "middle" }}>Business Services</td>
+                              <td class="font-size-sm" style={{ textAlign: "center", border: "1px solid #161c2d", padding: "0.5rem", verticalAlign: "middle" }}>4,322</td>
+                              <td class="font-size-sm" style={{ textAlign: "center", border: "1px solid #161c2d", padding: "0.5rem", verticalAlign: "middle" }}>5,937</td>
+                              <td class="font-size-sm" style={{ textAlign: "center", border: "1px solid #161c2d", padding: "0.5rem", verticalAlign: "middle" }}>7,611</td>
+                              <td class="font-size-sm" style={{ textAlign: "center", border: "1px solid #161c2d", padding: "0.5rem", verticalAlign: "middle" }}>4,322</td>
+                              <td class="font-size-sm" style={{ textAlign: "center", border: "1px solid #161c2d", padding: "0.5rem", verticalAlign: "middle" }}>6,055</td>
+                              <td class="font-size-sm" style={{ textAlign: "center", border: "1px solid #161c2d", padding: "0.5rem", verticalAlign: "middle" }}>7,937</td>
+                            </tr>
+
+                            <tr>
+                              <td class="font-size-sm" style={{ textAlign: "center", border: "1px solid #161c2d", padding: "0.5rem", verticalAlign: "middle" }}>Education, Health & Social Services</td>
+                              <td class="font-size-sm" style={{ textAlign: "center", border: "1px solid #161c2d", padding: "0.5rem", verticalAlign: "middle" }}>3,358</td>
+                              <td class="font-size-sm" style={{ textAlign: "center", border: "1px solid #161c2d", padding: "0.5rem", verticalAlign: "middle" }}>4,179</td>
+                              <td class="font-size-sm" style={{ textAlign: "center", border: "1px solid #161c2d", padding: "0.5rem", verticalAlign: "middle" }}>5,579</td>
+                              <td class="font-size-sm" style={{ textAlign: "center", border: "1px solid #161c2d", padding: "0.5rem", verticalAlign: "middle" }}>3,358</td>
+                              <td class="font-size-sm" style={{ textAlign: "center", border: "1px solid #161c2d", padding: "0.5rem", verticalAlign: "middle" }}>4,229</td>
+                              <td class="font-size-sm" style={{ textAlign: "center", border: "1px solid #161c2d", padding: "0.5rem", verticalAlign: "middle" }}>5,579</td>
+                            </tr>
 
   		                  </table>
 	     	            	</div>
@@ -54,9 +139,9 @@ export default function AdditionalBuyersStampDuty() {
        	                  <div className="list-group list-group-flush">
                             <div className="list-group-item d-flex align-items-center">
                               <div className="mr-auto">
-                                <a href="https://www.iras.gov.sg/IRASHome/Other-Taxes/Stamp-Duty-for-Property/Working-out-your-Stamp-Duty/Buying-or-Acquiring-Property/What-is-the-Duty-that-I-Need-to-Pay-as-a-Buyer-or-Transferee-of-Residential-Property/Additional-Buyer-s-Stamp-Duty--ABSD-/">
+                                <a href="https://stats.mom.gov.sg/Pages/Occupational-Wages-Tables2019.aspx">
                                 <p className="font-weight-bold mb-1">
-                                	IRAS | Additional Buyer's Stamp Duty (BSD) <sup><span data-toggle="tooltip" data-placement="top" title="" data-original-title="Navigation/Up-right.svg"><svg width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><g fill="none" fillRule="evenodd"><path d="M0 0h24v24H0z"></path><rect fill="#335EEA" opacity=".3" transform="rotate(45 11.646 12.854)" x="10.646" y="5.854" width="2" height="14" rx="1"></rect><path d="M8.11 8.904a1 1 0 110-2h8.486a1 1 0 011 .956l.353 8.132a1 1 0 11-1.998.087l-.312-7.175H8.11z" fill="#335EEA"></path></g></svg></span></sup>
+                                	Table: Occupational Wages 2019 <sup><span data-toggle="tooltip" data-placement="top" title="" data-original-title="Navigation/Up-right.svg"><svg width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><g fill="none" fillRule="evenodd"><path d="M0 0h24v24H0z"></path><rect fill="#335EEA" opacity=".3" transform="rotate(45 11.646 12.854)" x="10.646" y="5.854" width="2" height="14" rx="1"></rect><path d="M8.11 8.904a1 1 0 110-2h8.486a1 1 0 011 .956l.353 8.132a1 1 0 11-1.998.087l-.312-7.175H8.11z" fill="#335EEA"></path></g></svg></span></sup>
                                 </p>
                                 </a>
                               </div>
